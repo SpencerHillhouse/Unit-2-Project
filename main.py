a6be1af90a21570c5de10ef843892a7321659a0c
Quan <jbuford@basecampcodingacademy.org>
2024-07-29 10:39:37 -0500

    basic structure

---
+++ main.py
@@ -0,0 +1,27 @@
+from dataclasses import dataclass
+
+@dataclass
+class System :
+    supervisor : str
+    employee : str
+
+def timesheet():
+    ...
+
+def assignTask():
+    ...
+
+def taskProgress():
+    ...
+
+def viewTask():
+    ...
+
+def main():
+    ...
+
+
+if __name__ == "__main__":
+    main()
+
+#Quan did the basic structure can change as needed!!
